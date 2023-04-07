f6f81add7d8c12f3c1817e1283ba58f52983732d
laidouni <mohamed.laidouni@etu.u-paris.fr>
2023-04-07 12:36:42 +0200

    debut fonctions noeud.c

--- noeud.c
+++ noeud.c
@@ -19,28 +19,33 @@ noeud *delete_noeud(noeud *node)
 
 noeud *search_noeud_list(liste_noeud *ln, char *nom)
 {
-    if (ln == NULL)
+    if (ln == NULL) // Cas 1 : le dossier n'exite pas.
         return NULL;
-    else if (ln->no->nom == nom && !(ln->no->est_dossier))
+    else if (ln->no->nom == nom && !(ln->no->est_dossier)) // Cas 2 : [Erreur] Le nom est un fichier.
     {
         printf("Erreur : %s n'est pas un dossier.", nom);
         exit(EXIT_FAILURE);
     }
-    else if (ln->no->nom == nom)
+    else if (ln->no->nom == nom) // Cas 3 : On trouve le nom.
         return ln->no;
     else
-        return search_noeud_list(ln->succ, nom);
+        return search_noeud_list(ln->succ, nom); // Sinon on regarde dans les autres fils.
 }
 
 noeud *search_noeud(noeud *n, char *nom)
 {
-    if (n == NULL)
+    if (n == NULL) // Cas 1 : Le dossier n'exite pas.
         return NULL;
-    else if (n->nom == nom)
+    else if (n->nom == nom && !(n->est_dossier)) // Cas 2 : [Erreur] Le nom est un fichier.
+    {
+        printf("Erreur : %s n'est pas un dossier.", nom);
+        exit(EXIT_FAILURE);
+    }
+    else if (n->nom == nom) // Cas 3 : On trouve le dossier.
         return n;
-    else if (search_noeud_list(n->fils, nom) != NULL)
+    else if (search_noeud_list(n->fils, nom) != NULL) // Cas 4 : Le dossier est un des fils.
         return search_noeud_list(n->fils, nom);
-    else
+    else // Le dossier n'est ni le dossier courant, ni parmi les fils.
     {
     }
 }
