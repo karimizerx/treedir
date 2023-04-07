fefb4dad2ae0bc420d1b2420a25b68ccda0d7bef
Boudelaa <fares.boudelaa@etu.u-paris.fr>
2023-04-07 17:40:09 +0200

    lecture

--- lecture.h
+++ lecture.h
@@ -1,3 +1,13 @@
 // Fares
-// char **split(char *ligneDuFichier)
-// execute()
+
+//renvoi un tableau de dim 3 
+extern char **split(char *ligne); 
+
+//execute la commande 
+extern void execute(char* command, char* arg1, char* arg2);
+
+//renvoi le prochain mot
+extern char *next(char *ligne);
+
+//lis le fichier
+extern void read(char* filename);
