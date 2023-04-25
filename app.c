e167bb3c33e4412c8402ace9abb8fd42da09c1a0
laidouni <mohamed.laidouni@etu.u-paris.fr>
2023-04-25 20:56:12 +0200

    cd foncitonnel, presque sans leaks (leak still reachable => cause à déterminer)

--- app.c
+++ app.c
@@ -140,12 +140,34 @@ int main(int argc, char const *argv[])
 
     //  Déplacement dans un autre noeud.
     puts("Déplacement dans un autre noeud.");
-    noeud *courant = n0;
+    noeud ncourant = *n0;
+    noeud *courant = &ncourant;
     printf("Noeud courant : ");
     pwd(courant);
     cd(&courant, "Cours/ProjetC");
-    printf("Noeud courant après cd : ");
+    printf("Noeud courant après cd 'Cours/ProjetC' : ");
     pwd(courant);
+    cd(&courant, "/Cours");
+    printf("Noeud courant après cd '/Cours: ");
+    pwd(courant);
+    // // cd(&courant, "TP");
+    // // printf("Noeud courant après cd 'TP' : ");
+    // // pwd(courant);
+    // cd(&courant, "ProjetC");
+    // printf("Noeud courant après cd 'ProjetC' : ");
+    // pwd(courant);
+    // cd(&courant, "/TD");
+    // printf("Noeud courant après cd '/TD' : ");
+    // pwd(courant);
+    cd(&courant, ".");
+    printf("Noeud courant après cd '.' : ");
+    pwd(courant);
+    cd(&courant, "..");
+    printf("Noeud courant après cd '..' : ");
+    pwd(courant);
+    // cd(&courant, "TD/td1");
+    // printf("Noeud courant après cd 'TD/td1' : ");
+    // pwd(courant);
 
     // On libère toutes les ressources.
     free_noeud(n0);
--- commande.c
+++ commande.c
@@ -25,10 +25,7 @@ void cd(noeud **courant, char *chemin)
 {
     noeud *tmp;
     if (chemin == NULL) // Cas 0 : Le dossier demandé est la racine.
-    {
         *courant = (*courant)->racine;
-        // return courant; // On renvoie ce dossier.
-    }
     else
     {
         if (*chemin == '/') // Cas 1 : Le chemin est absolue.
@@ -40,15 +37,16 @@ void cd(noeud **courant, char *chemin)
         if (tmp != NULL && tmp->est_dossier) // Cas 2 : On trouve le noeud demandé & c'est bien un dossier.
         {
             *courant = tmp;
-            // return courant; // On renvoie ce dossier.
         }
         else
         {
-            if (!search_noeud(courant, chemin)->est_dossier) // Cas 3.1 (ERREUR): Le chemin renvoie vers un fichier.
-                printf("Erreur dans 'cd' : Le chemin '%s' pointe vers un fichier.\n", chemin);
-            else // Cas 3.2 (ERREUR) : Le chemin n'existe pas.
-                printf("Erreur dans 'cd' : Le chemin '%s' n'existe pas.\n", chemin);
-            exit(EXIT_FAILURE); // On arrête donc le programme.
+            if (tmp == NULL) // Cas 3.1 (ERREUR): Le chemin n'existe pas.
+                printf("Erreur dans 'cd' (ligne 39) : Le chemin '%s' n'existe pas.\n", chemin);
+            else // Cas 3.2 (ERREUR) : Le chemin renvoie vers un fichier.
+            {
+                printf("Erreur dans 'cd' (ligne 39) : Le chemin '%s' pointe vers un fichier.\n", chemin);
+                exit(EXIT_FAILURE); // On arrête donc le programme.
+            }
         }
     }
 }
